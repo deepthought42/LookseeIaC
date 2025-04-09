a55e04294333d494ab522dd0491cd630d2a50033
Brandon Kindred <brandon.kindred@gmail.com>
2025-04-09 00:28:51 -0400

    chore(): moved iam to own file

--- cloud_run/iam.tf
+++ cloud_run/iam.tf
@@ -1,6 +1,7 @@
 # Create a service account for Cloud Run
 resource "google_service_account" "cloud_run_pubsub" {
-  account_id   = "cloud-run-pubsub-sa"
+  project      = var.project_id
+  account_id   = "${var.service_account_name}-cloud-run-pubsub-sa"
   display_name = "Cloud Run PubSub Publisher Service Account"
 }
 
--- cloud_run/main.tf
+++ cloud_run/main.tf
@@ -1,16 +1,3 @@
-# Create service account for Cloud Run
-resource "google_service_account" "cloud_run_sa" {
-  project      = var.project_id
-  account_id   = var.service_account_name
-  display_name = "Service Account for Cloud Run with PubSub permissions"
-}
-
-# Grant pubsub publisher role to service account
-resource "google_project_iam_member" "pubsub_publisher" {
-  project = var.project_id
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
-}
 
 # Create Cloud Run service
 resource "google_cloud_run_service" "service" {
